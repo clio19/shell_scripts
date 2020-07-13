66b27d64054736aa2ea96e9c2d4cfa649fa93ce6
Htrix <clio19@users.noreply.github.com>
2020-07-13 12:58:29 +0100

    Create additional JetBrains products.ps1

---
+++ additional JetBrains products.ps1
@@ -0,0 +1,8 @@
+$processExclusions.Add('webstorm64.exe') > $null
+$processExclusions.Add('datagrip64.exe') > $null
+$processExclusions.Add('clion64.exe') > $null
+$processExclusions.Add('goland64.exe') > $null
+$processExclusions.Add('phpstorm64.exe') > $null
+$processExclusions.Add('pycharm64.exe') > $null
+$processExclusions.Add('rubymine64.exe') > $null
+$processExclusions.Add('studio64.exe') > $null
